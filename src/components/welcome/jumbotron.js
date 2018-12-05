44a3ad4392b5ecab31f1db19865b6a5bea278e15
Andy Chen <amazingandyyy@gmail.com>
2018-12-04 22:56:48 -0800

    netlify works

--- src/components/welcome/jumbotron.js
+++ src/components/welcome/jumbotron.js
@@ -4,7 +4,7 @@ export default class Jumbotron extends React.Component {
   render() {
     return (<div className='jumbotron'>
       <img src='assets/logo-white.svg' className='logo'/>
-      {/* <p>Solutions for Blockchain Infrastructures</p> */}
+      <p>Solutions for Blockchain Infrastructures</p>
     </div>)
   }
 }
